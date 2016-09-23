9f88e470edbf358783387ced7a2fc9d824efadc7
唐松 <tangsong@camera360.com>
2016-09-23 12:46:12 +0800

    编写自己的第一个类

---
+++ Learn_OC/TSPerson.h
@@ -0,0 +1,20 @@
+//
+//  TSPersion.h
+//  Learn_OC
+//
+//  Created by tangsong on 9/21/16.
+//  Copyright © 2016 tangsong. All rights reserved.
+//
+
+#ifndef TSPerson_h
+#define TSPerson_h
+
+@interface TSPerson : NSObject
+@property NSString* name;
+@property int id;
+
+-(void) sayHello;
+
+@end
+
+#endif /* TSPerson_h */
---
+++ Learn_OC/TSPerson.m
@@ -0,0 +1,18 @@
+//
+//  TSPersion.m
+//  Learn_OC
+//
+//  Created by tangsong on 9/21/16.
+//  Copyright © 2016 tangsong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "TSPerson.h"
+
+@implementation TSPerson
+
+-(void) sayHello {
+     NSLog(@"%s", "334");
+}
+
+@end
