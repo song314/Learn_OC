75d84e67aa5cb7ea3f73e51f3e83bacc0d1ee6a9
唐松 <tangsong@camera360.com>
2016-09-23 12:45:40 +0800

    在main里面调用自己的类

--- Learn_OC/main.m
+++ Learn_OC/main.m
@@ -7,11 +7,32 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "TSPerson.h"
 
 int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+
+    NSLog(@"Hello, World!");
+        
+    TSPerson* firstPerson = [[TSPerson alloc] init];
+//     TSPersion* pTsP = [TSPersion new]; 当使用无参数构造方法时
+
+    [firstPerson sayHello];
+    [firstPerson setName:@"tang song"];
+    
+    TSPerson* p2rdPerson = [TSPerson new];
+    [p2rdPerson setName:@" 22222 name"];
+    
+    
+    NSLog(@"My name is : %@", [firstPerson name]);
+    NSLog(@"My name is : %@", [p2rdPerson name]);
+    
+    
+    if ([firstPerson isEqual:p2rdPerson]) {
+        NSLog(@" we are the same");
+    } else {
+        NSLog(@" we are different ");
     }
+        
+
     return 0;
 }
